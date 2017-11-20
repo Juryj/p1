f6e8922ba913d5346291c737694964ace9a14a11
jk5.name <jk5.name@gmail.com>
2017-11-20 15:03:59 +0200

    Modified

--- db/db_counter.sql
+++ db/db_counter.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50720
  File Encoding         : 65001
 
- Date: 09/11/2017 15:57:01
+ Date: 20/11/2017 14:49:15
 */
 
 SET NAMES utf8mb4;
@@ -24,12 +24,13 @@ DROP TABLE IF EXISTS `counter`;
 CREATE TABLE `counter`  (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `counter` bigint(255) NOT NULL,
+  `url` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of counter
 -- ----------------------------
-INSERT INTO `counter` VALUES (1, 1);
+INSERT INTO `counter` VALUES (1, 1, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
--- www/banner.php
+++ www/banner.php
@@ -1,33 +1,34 @@
 <?php
 
-function mysql_connect ($server = 'ini_get("localhost")', $username = 'ini_get("mysql")', $password = 'ini_get("mysql.default_password")', $new_link = false, $client_flags = 0) {}
-function mysql_select_db ($database_name, $link_identifier = null) {}
-
-//Конект к базе
-mysql_connect("localhost", "root", "900020jWP#") or die(mysql_error()) ;
-mysql_select_db("users") or die(mysql_error()) ; //
-
-
-$dbcounter = mysql_connect($hostname, $username, $password)
-or die("Unable to connect to MySQL");
-echo "Connected to MySQL<br>";
-
-    if (empty($_GET['id'])) {
-        header('HTTP/1.1 404 Not found');
-        die();
-    } else {
-
-        $id = mysql_real_escape_string($_GET['id']);
-        $result = mysql_query("select url from users where id = '$id'");
-
-        if (!mysql_num_rows($result)) {
-            header('HTTP/1.1 404 Not found');
-            die();
+class db {
+    function getConnection()
+    {
+        try {
+            $db = new PDO("mysql:host=localhost;dbname=db_counter","root","900020jWP#");
         }
-
-        $url = mysql_result($result, 0);
-
-        mysql_query("update users set clicks = clicks + 1 where id = '$id'");
-        header('Location: http://'.$url);
-
+        catch(PDOException $e) {
+            die($e->getMessage());
+        }
+        $db->exec("set names utf8");
+        return $db;
     }
+}
+
+$db = db::getConnection();
+$url = $_SERVER['REQUEST_URI'];
+
+/* проверяем есть ли уже такой url в базе  */
+$result = $db->prepare("SELECT * FROM counter WHERE url='$url'");
+$result->execute();
+
+if($result->rowCount() > 0){
+    while($res = $result->fetch(PDO::FETCH_BOTH)){
+        $row['id'] = $res['id'];
+    }
+    $result = $db->prepare("UPDATE counter set counter = counter + 1 WHERE id=" . $row['id']);
+    $result->execute();
+
+} else {
+    $result = $db->prepare("INSERT INTO counter (url, counter) VALUES ('$url', 1)");
+    $result->execute();
+}
